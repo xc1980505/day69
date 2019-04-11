dca0a7de246817c2405fafe819e3c22231590c84
xc1988 <416931874@qq.com>
2019-04-11 17:43:33 +0800

    add info

--- src/com/qianfeng/test/App.java
+++ src/com/qianfeng/test/App.java
@@ -3,5 +3,7 @@ package com.qianfeng.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("hha");
+        System.out.println("hehe");
+        System.out.println("heihei");
     }
 }
